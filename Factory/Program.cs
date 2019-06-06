5dcbb30a1801f75a0940b37adf9b8c937b9f3382
Burak Parlak <burak.parlak90@hotmail.com>
2019-06-07 00:50:25 +0300

    Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nesneleri factory dizayn pattern ile üreterek kodumuzu değişime açık hale getirdik. 
+            CustomerManager cm = new CustomerManager(new FileLoggerFactory());
+            cm.Save();
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Birkaç farklı yere(database, dosyalar, mail), birkaç farklı şekilde(xml,json) loglama yapılabilir.
+    /// We can record the log with diffirent file format(xml,json) to different places(to databases, to file, to mailbox)
+    /// </summary>
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+    public class DbLoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Loglama ile ilgili işlemler burada 
+            return new DbLogger();
+        }
+    }
+    public class FileLoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Loglama ile ilgili işlemler burada
+            return new FileLogger();
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class DbLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with DatabaseLogger");
+        }
+    }
+    public class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with FileLogger");
+        }
+    }
+
+    //iş katmanı
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,7 +6,37 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 3; i++)
+            {
+                var getInstance = CustomerManager.GetInstance();
+                getInstance.Save();
+            }
+            Console.ReadKey();
+        }
+    }
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+        private CustomerManager()
+        {
+            Console.WriteLine("Instance created.");
+        }
+        public static CustomerManager GetInstance()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            Console.WriteLine("GetInstance running.");
+            return _customerManager;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
         }
     }
 }
